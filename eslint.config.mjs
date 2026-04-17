fe556e90c64340d6dbe6cbd7c7128f74d88f2db9
Misti <gekgrind@gmail.com>
2026-04-17 12:29:04 -0400

    update error tracking

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,20 @@
-import { defineConfig, globalIgnores } from "eslint/config"
-import nextVitals from "eslint-config-next/core-web-vitals"
-import nextTs from "eslint-config-next/typescript"
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-])
+
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
